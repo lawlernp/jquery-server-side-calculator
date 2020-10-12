a28d3798fb9fd83cda974af73477f5f4a732346d
lawlernp <lawlernp@gmail.com>
2020-10-12 06:09:13 -0500

    added logic for server calc

--- server/server.js
+++ server/server.js
@@ -10,6 +10,59 @@ let calcArray
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function calculate(array) {
+    let equation = array[array.length - 1].objectToCalculate;
+    if (equation.operator === '+') {
+        let answer = Number(equation.inputOne) + Number(equation.inputTwo);
+        let answerObj = {
+            inputOne: equation.inputOne,
+            operator: equation.operator,
+            inputTwo: equation.inputTwo,
+            answer: answer
+        }
+        currentCalculation.pop;
+        calculationHistory.push(answerObj);
+        return answerObj;
+    }
+    else if (equation.operator === '-') {
+        let answer = Number(equation.inputOne) - Number(equation.inputTwo);
+        let answerObj = {
+            inputOne: equation.inputOne,
+            operator: equation.operator,
+            inputTwo: equation.inputTwo,
+            answer: answer
+        }
+        currentCalculation.pop;
+        calculationHistory.push(answerObj);
+        return answerObj;
+    }
+    else if (equation.operator === '*') {
+        let answer = Number(equation.inputOne) * Number(equation.inputTwo);
+        let answerObj = {
+            inputOne: equation.inputOne,
+            operator: equation.operator,
+            inputTwo: equation.inputTwo,
+            answer: answer
+        }
+        currentCalculation.pop;
+        calculationHistory.push(answerObj);
+        return answerObj;
+    }
+    else if (equation.operator === '/') {
+        let answer = Number(equation.inputOne) / Number(equation.inputTwo);
+        let answerObj = {
+            inputOne: equation.inputOne,
+            operator: equation.operator,
+            inputTwo: equation.inputTwo,
+            answer: answer
+        }
+        currentCalculation.pop;
+        calculationHistory.push(answerObj);
+        return answerObj;
+    }
+};
+
+
 
 app.listen(port, () => {
     console.log('up and running on port : ', port);
